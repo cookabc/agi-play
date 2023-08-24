09ceae01be0baba49a1e94bc1b65cc19ebba7306
cookabc <chuangcius@gmail.com>
2023-08-24 18:54:30 +0800

    put schemas in items.py

--- api/models/items.py
+++ api/models/items.py
@@ -1,8 +1,25 @@
+from pydantic import BaseModel
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, Session
 
-from api.models import schemas
-from api.database import Base
+from ..database import Base
+
+
+class ItemBase(BaseModel):
+    title: str
+    description: str | None = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class ItemObject(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
 
 
 class Item(Base):
@@ -20,7 +37,7 @@ def get_items(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Item).offset(skip).limit(limit).all()
 
 
-def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
+def create_user_item(db: Session, item: ItemCreate, user_id: int):
     db_item = Item(**item.dict(), owner_id=user_id)
     db.add(db_item)
     db.commit()
