84c27be4710f90937f456c6cefb94805f2934ea4
cookabc <chuangcius@gmail.com>
2023-08-24 18:31:20 +0800

    use separate Item file

---
+++ api/items.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, Session
+
+from . import schemas
+from .database import Base
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="items")
+
+
+def get_items(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(Item).offset(skip).limit(limit).all()
+
+
+def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
+    db_item = Item(**item.dict(), owner_id=user_id)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
