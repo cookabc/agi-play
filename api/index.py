5b8be2a1b58910c15a251964052f813e7ba15d1d
cookabc <chuangcius@gmail.com>
2023-08-24 18:53:40 +0800

    remove schemas from the Main file

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@ from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
 from .database import Base, SessionLocal, engine
-from .models import users as USER, items as ITEM, schemas
+from .models import users as USER, items as ITEM
 
 Base.metadata.create_all(bind=engine)
 
@@ -23,21 +23,21 @@ def hello_world():
     return "Hello World"
 
 
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+@app.post("/users/", response_model=USER.UserObject)
+def create_user(user: USER.UserCreate, db: Session = Depends(get_db)):
     db_user = USER.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return USER.create_user(db=db, user=user)
 
 
-@app.get("/users/", response_model=list[schemas.User])
+@app.get("/users/", response_model=list[USER.UserObject])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = USER.get_users(db, skip=skip, limit=limit)
     return users
 
 
-@app.get("/users/{user_id}", response_model=schemas.User)
+@app.get("/users/{user_id}", response_model=USER.UserObject)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = USER.get_user(db, user_id=user_id)
     if db_user is None:
@@ -45,12 +45,12 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@app.post("/users/{user_id}/items/", response_model=schemas.Item)
-def create_item_for_user(user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
+@app.post("/users/{user_id}/items/", response_model=ITEM.ItemCreate)
+def create_item_for_user(user_id: int, item: ITEM.ItemCreate, db: Session = Depends(get_db)):
     return ITEM.create_user_item(db=db, item=item, user_id=user_id)
 
 
-@app.get("/items/", response_model=list[schemas.Item])
+@app.get("/items/", response_model=list[ITEM.ItemObject])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = ITEM.get_items(db, skip=skip, limit=limit)
     return items
