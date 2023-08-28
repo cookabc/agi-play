1534b41ed67264ab7bf5ace51aa082e3fbf4f20a
cookabc <chuangcius@gmail.com>
2023-08-28 11:45:14 +0800

    update the Main file

--- api/index.py
+++ api/index.py
@@ -1,56 +1,16 @@
-from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
 
-from .database import Base, SessionLocal, engine
-from .models import users as USER, items as ITEM
+from .database import Base, engine
+from .routers import items, users
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+app.include_router(items.router)
+app.include_router(users.router)
 
 
 @app.get("/")
 def hello_world():
     return "Hello World"
-
-
-@app.post("/users/", response_model=USER.UserObject)
-def create_user(user: USER.UserCreate, db: Session = Depends(get_db)):
-    db_user = USER.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return USER.create_user(db=db, user=user)
-
-
-@app.get("/users/", response_model=list[USER.UserObject])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = USER.get_users(db, skip=skip, limit=limit)
-    return users
-
-
-@app.get("/users/{user_id}", response_model=USER.UserObject)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = USER.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-
-@app.post("/users/{user_id}/items/", response_model=ITEM.ItemCreate)
-def create_item_for_user(user_id: int, item: ITEM.ItemCreate, db: Session = Depends(get_db)):
-    return ITEM.create_user_item(db=db, item=item, user_id=user_id)
-
-
-@app.get("/items/", response_model=list[ITEM.ItemObject])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = ITEM.get_items(db, skip=skip, limit=limit)
-    return items
