3eff749b3403500ff8daead0bf0868bce27ca89a
cookabc <chuangcius@gmail.com>
2023-09-07 18:45:16 +0800

    add train into index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // One frame is ~23ms of audio.
+let recognizer;
 const NUM_FRAMES = 3;
 let examples = [];
-let recognizer;
 
 function collect(label) {
     if (recognizer.isListening()) {
@@ -26,6 +26,59 @@ function normalize(x) {
     const mean = -100;
     const std = 10;
     return x.map(x => (x - mean) / std);
+}
+
+const INPUT_SHAPE = [NUM_FRAMES, 232, 1];
+let model;
+
+async function train() {
+    toggleButtons(false);
+    const ys = tf.oneHot(examples.map(e => e.label), 3);
+    const xsShape = [examples.length, ...INPUT_SHAPE];
+    const xs = tf.tensor(flatten(examples.map(e => e.vals)), xsShape);
+
+    await model.fit(xs, ys, {
+        batchSize: 16,
+        epochs: 10,
+        callbacks: {
+            onEpochEnd: (epoch, logs) => {
+                document.querySelector('#console').textContent =
+                    `Accuracy: ${(logs.acc * 100).toFixed(1)}% Epoch: ${epoch + 1}`;
+            }
+        }
+    });
+    tf.dispose([xs, ys]);
+    toggleButtons(true);
+}
+
+function buildModel() {
+    model = tf.sequential();
+    model.add(tf.layers.depthwiseConv2d({
+        depthMultiplier: 8,
+        kernelSize: [NUM_FRAMES, 3],
+        activation: 'relu',
+        inputShape: INPUT_SHAPE
+    }));
+    model.add(tf.layers.maxPooling2d({poolSize: [1, 2], strides: [2, 2]}));
+    model.add(tf.layers.flatten());
+    model.add(tf.layers.dense({units: 3, activation: 'softmax'}));
+    const optimizer = tf.train.adam(0.01);
+    model.compile({
+        optimizer,
+        loss: 'categoricalCrossentropy',
+        metrics: ['accuracy']
+    });
+}
+
+function toggleButtons(enable) {
+    document.querySelectorAll('button').forEach(b => b.disabled = !enable);
+}
+
+function flatten(tensors) {
+    const size = tensors[0].length;
+    const result = new Float32Array(tensors.length * size);
+    tensors.forEach((arr, i) => result.set(arr, i * size));
+    return result;
 }
 
 function predictWord() {
