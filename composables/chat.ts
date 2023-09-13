b275538aad9f310186a58da477371d5de8e23f1a
cookabc <chuangcius@gmail.com>
2023-09-13 20:07:34 +0800

    add composable: chat.ts

---
+++ composables/chat.ts
@@ -0,0 +1,17 @@
+// 获取历史对话
+import {Response} from "~/types/response";
+
+export async function getChatList(session_id: string) {
+    return await $fetch('/api/chat/list', {
+        method: 'POST',
+        query: {session_id},
+    }) as Response
+}
+
+// 对话
+export async function makeChat(body: any) {
+    return await $fetch('/api/chat/do', {
+        method: 'POST',
+        body: body,
+    }) as Response
+}
