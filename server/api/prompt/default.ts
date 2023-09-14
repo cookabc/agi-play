c3115a71620f469a047cdd664b9c96c47699adc4
cookabc <chuangcius@gmail.com>
2023-09-14 12:51:28 +0800

    use sqlite DB for APIs

--- server/api/prompt/default.ts
+++ server/api/prompt/default.ts
@@ -1,3 +1,21 @@
+import {getRows} from "~/database";
+import {Response} from "~/types/response";
+
 export default defineEventHandler(async (event) => {
-    return await $fetch(useRuntimeConfig().baseURL + '/api/v2/prompt/default')
+    let sql = `
+        SELECT p.id,
+               pc.name    as category,
+               pc."order" as category_order,
+               p.title,
+               p.summary,
+               p.prompt,
+               p."order"  as prompt_order,
+               p.hot,
+               FALSE
+        FROM prompt p
+                 JOIN prompt_category pc ON p.category_id = pc.id
+        WHERE p.hot = true
+          AND p.deleted_at is null;
+    `
+    return {code: 0, data: await getRows(sql), message: 'OK'} as Response;
 });
--- server/api/prompt/list.ts
+++ server/api/prompt/list.ts
@@ -1,7 +1,23 @@
+import {getRows} from '~/database';
+import {Response} from "~/types/response";
+
 export default defineEventHandler(async (event) => {
-    return await $fetch(useRuntimeConfig().baseURL + '/api/v2/prompt/list',
-        {
-            params: {name: getQuery(event).name}
-        }
-    )
+    let sql = `
+        SELECT p.id,
+            pc.name    as category,
+            pc."order" as category_order,
+            p.title,
+            p.summary,
+            p.prompt,
+            p."order"  as prompt_order,
+            p.hot
+        FROM prompt p
+        JOIN prompt_category pc ON p.category_id = pc.id
+        WHERE p.deleted_at is null
+    `
+    const name = getQuery(event).name
+    if (name) {
+        sql += ` AND (p.title like '%${name}%' or p.summary like '%${name}%' or p.prompt like '%${name}%')`
+    }
+    return {code: 0, data: await getRows(sql), message: 'OK'} as Response;
 });
