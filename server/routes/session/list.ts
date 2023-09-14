320074c01acd72a277b34f7f34fd9ac9d091ba92
cookabc <chuangcius@gmail.com>
2023-09-14 12:51:01 +0800

    use json file as DB for API: session/list

--- server/routes/session/list.ts
+++ server/routes/session/list.ts
@@ -1,23 +1,5 @@
-import {getRows} from "~/database";
 import {Response} from "~/types/response";
 
 export default defineEventHandler(async (event) => {
-    let sql = ''
-    const name = getQuery(event).name
-    if (name) {
-        sql = `
-            SELECT id, name, created_at, updated_at
-            FROM session
-            WHERE name LIKE %s AND deleted_at IS NULL
-            ORDER BY updated_at DESC
-        `
-    } else {
-        sql = `
-            SELECT id, name, created_at, updated_at
-            FROM session
-            WHERE deleted_at IS NULL
-            ORDER BY updated_at DESC
-        `
-    }
-    return {code: 0, data: await getRows(sql), message: 'OK'} as Response;
+    return {code: 0, data: [], message: 'OK'} as Response;
 });
