384ee58aafdef089801cc58c8626867a135bc9cb
cookabc <chuangcius@gmail.com>
2023-09-14 12:07:06 +0800

    add: sqlite3 DB client

---
+++ database/index.ts
@@ -0,0 +1,33 @@
+import sqlite3 from 'sqlite3';
+
+export const DB = new sqlite3.Database('app.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the APP database.');
+});
+
+
+export const getRows = function (sql: string) {
+    return new Promise(function (resolve, reject) {
+        DB.all(sql, (err, rows) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(rows);
+            }
+        });
+    });
+}
+
+export const getRow = function (sql: string) {
+    return new Promise(function (resolve, reject) {
+        DB.get(sql, (err, row) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(row);
+            }
+        });
+    });
+}
