960ed0d2282c0173af78f657050e6776da72d251
cookabc <chuangcius@gmail.com>
2023-09-14 12:48:20 +0800

    use json file as DB for API: prompt/list

--- server/routes/prompt/list.ts
+++ server/routes/prompt/list.ts
@@ -1,23 +1,6 @@
-import {getRows} from '~/database';
+import promptList from '~/database/prompt.list.json';
 import {Response} from "~/types/response";
 
 export default defineEventHandler(async (event) => {
-    let sql = `
-        SELECT p.id,
-            pc.name    as category,
-            pc."order" as category_order,
-            p.title,
-            p.summary,
-            p.prompt,
-            p."order"  as prompt_order,
-            p.hot
-        FROM prompt p
-        JOIN prompt_category pc ON p.category_id = pc.id
-        WHERE p.deleted_at is null
-    `
-    const name = getQuery(event).name
-    if (name) {
-        sql += ` AND (p.title like '%${name}%' or p.summary like '%${name}%' or p.prompt like '%${name}%')`
-    }
-    return {code: 0, data: await getRows(sql), message: 'OK'} as Response;
+    return {code: 0, data: promptList, message: 'OK'} as Response;
 });
