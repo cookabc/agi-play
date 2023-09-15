e651823ffa431f9d00226218910d3482ca46b8a2
cookabc <chuangcius@gmail.com>
2023-09-15 13:42:53 +0800

    scrollToBottom

--- components/ChatComponent.vue
+++ components/ChatComponent.vue
@@ -1,53 +1,56 @@
 <template>
-  <div v-if="state.loading" class="absolute w-full h-full flex justify-center items-center text-3xl">
-    <loading-outlined/>
-  </div>
-  <div v-else>
-    <template v-if="state.messages.length > 0" v-for="message of state.messages" :key="message.id">
-      <div class="group message-item w-full">
-        <div
-            class="flex p-4 gap-2 text-base md:gap-6 md:max-w-2xl lg:max-w-[38rem] xl:max-w-5xl md:py-4 lg:px-0 m-auto">
-          <div class="flex-shrink-0 flex flex-col relative items-end">
-            <div class="w-10">
-              <img src="@/assets/img/people.jpg" class="w-full h-full object-cover" alt=""/>
-            </div>
-          </div>
-          <div class="message-content relative bg-[var(--ant-primary-color-3)] flex flex-col gap-1 md:gap-3 p-4
-                      lg:w-[calc(100%-115px)] rounded-xl">
-            <text-component :text="message?.prompt"/>
-            <div class="flex">
-              <a-button type="text" size="small" class="ml-auto" title="Copy" @click="onCopy(message.prompt)">
-                <copy-outlined/>
-              </a-button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="group message-item w-full">
-        <div
-            class="flex p-4 gap-2 text-base md:gap-6 md:max-w-2xl lg:max-w-[38rem] xl:max-w-5xl md:py-4 lg:px-0 m-auto">
-          <div class="flex-shrink-0 flex flex-col relative items-end">
-            <div class="w-10">
-              <img src="@/assets/img/ai_robot.svg" class="w-full h-full object-cover" alt=""/>
-            </div>
-          </div>
+  <div class="message-list py-4" id="scrollRef">
+    <div v-if="state.loading" class="absolute w-full h-full flex justify-center items-center text-3xl">
+      <loading-outlined/>
+    </div>
+    <div v-else>
+      <template v-if="state.messages.length > 0" v-for="message of state.messages" :key="message.id">
+        <div class="group message-item w-full">
           <div
-              class="message-content relative flex w-[calc(100%-50px)] flex-col lg:w-[calc(100%-115px)] bg-white p-4 rounded-xl">
-            <text-component :text="message?.response"/>
-            <div class="flex">
-              <a-button type="text" size="small" class="ml-auto" title="复制" @click="onCopy(message.response)">
-                <copy-outlined/>
-              </a-button>
+              class="flex p-4 gap-2 text-base md:gap-6 md:max-w-2xl lg:max-w-[38rem] xl:max-w-5xl md:py-4 lg:px-0 m-auto">
+            <div class="flex-shrink-0 flex flex-col relative items-end">
+              <div class="w-10">
+                <img src="@/assets/img/people.jpg" class="w-full h-full object-cover" alt=""/>
+              </div>
+            </div>
+            <div class="message-content relative bg-[var(--ant-primary-color-3)] flex flex-col gap-1 md:gap-3 p-4
+                      lg:w-[calc(100%-115px)] rounded-xl">
+              <text-component :text="message?.prompt"/>
+              <div class="flex">
+                <a-button type="text" size="small" class="ml-auto" title="Copy" @click="onCopy(message.prompt)">
+                  <copy-outlined/>
+                </a-button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </template>
-    <default-prompt v-else/>
+        <div class="group message-item w-full">
+          <div
+              class="flex p-4 gap-2 text-base md:gap-6 md:max-w-2xl lg:max-w-[38rem] xl:max-w-5xl md:py-4 lg:px-0 m-auto">
+            <div class="flex-shrink-0 flex flex-col relative items-end">
+              <div class="w-10">
+                <img src="@/assets/img/ai_robot.svg" class="w-full h-full object-cover" alt=""/>
+              </div>
+            </div>
+            <div
+                class="message-content relative flex w-[calc(100%-50px)] flex-col lg:w-[calc(100%-115px)] bg-white p-4 rounded-xl">
+              <text-component :text="message?.response"/>
+              <div class="flex">
+                <a-button type="text" size="small" class="ml-auto" title="复制" @click="onCopy(message.response)">
+                  <copy-outlined/>
+                </a-button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </template>
+      <default-prompt v-else/>
+    </div>
+    <div class="h-32 md:h-48 flex-shrink-0"></div>
   </div>
 </template>
 <script setup>
-import {onMounted, reactive} from 'vue'
+import {nextTick, onMounted, reactive} from 'vue'
 import {CopyOutlined, LoadingOutlined} from '@ant-design/icons-vue'
 import {copyText} from '~/utils/copy'
 import {message as Notify} from 'ant-design-vue'
@@ -81,6 +84,7 @@ const getChatList = async (session_id) => {
     console.warn('[ getChatList error ]', error)
   } finally {
     state.loading = false
+    await scrollToBottom()
   }
 }
 
@@ -91,6 +95,12 @@ const onCopy = (content) => {
   } else {
     Notify.error('复制失败');
   }
+}
+
+const scrollToBottom = async () => {
+  await nextTick()
+  const scrollRef = document.getElementById('scrollRef');
+  scrollRef.scrollIntoView(false);
 }
 
 onMounted(async () => {
