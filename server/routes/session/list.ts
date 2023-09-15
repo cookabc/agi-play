7fc6be7a7cfaff558da92de14e153a0b1d1713ab
cookabc <chuangcius@gmail.com>
2023-09-15 11:30:55 +0800

    add API: /session/list

--- server/routes/session/list.ts
+++ server/routes/session/list.ts
@@ -1,5 +1,6 @@
+import sessionList from '~/database/session.list.json';
 import {Response} from "~/types/response";
 
 export default defineEventHandler(async (event) => {
-    return {code: 0, data: [], message: 'OK'} as Response;
+    return {code: 0, data: sessionList, message: 'OK'} as Response;
 });
