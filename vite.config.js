de9c6dd4c5df970ab5fc1a6c5777d8b1079fcd44
cookabc <chuangcius@gmail.com>
2023-09-21 18:04:40 +0800

    add: Unocss plugin

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
 import path from 'path'
 
 /**
@@ -26,6 +27,7 @@ export default defineConfig(() => {
     return {
         plugins: [
             vue(),
+            Unocss({})
         ],
         resolve: {
             alias: {
