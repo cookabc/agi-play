e344b7c276aba9abdd47cf0af1494a8ae5745b5a
cookabc <chuangcius@gmail.com>
2023-10-09 12:34:21 +0800

    add router: /train/index/index.vue

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,14 @@ const router = createRouter({
             component: () => import("@/views/train/index.vue"),
             children: [
                 {
+                    path: '',
+                    name: 'TrainIndex',
+                    component: () => import("@/views/train/index/index.vue"),
+                    meta: {
+                        title: "项目列表"
+                    }
+                },
+                {
                     path: 'image',
                     name: 'TrainImage',
                     component: () => import("@/views/train/image/index.vue"),
