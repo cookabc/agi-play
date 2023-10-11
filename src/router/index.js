d41232a8c4f7c32f82a7b59f8b350158b2a2cd69
cookabc <chuangcius@gmail.com>
2023-10-11 15:06:01 +0800

    update: router

--- src/router/index.js
+++ src/router/index.js
@@ -9,17 +9,67 @@ const router = createRouter({
             component: () => import("@/views/index.vue")
         },
         {
+            path: '/project',
+            name: 'Project',
+            component: () => import("@/views/project/index.vue"),
+            meta: {
+                title: "项目"
+            },
+            children: [
+                {
+                    path: '',
+                    name: 'ProjectIndex',
+                    component: () => import("@/views/project/index/index.vue"),
+                    meta: {
+                        title: "项目列表"
+                    }
+                },
+                {
+                    path: '/prepare',
+                    name: 'ProjectPrepare',
+                    component: () => import("@/views/project/prepare/index.vue"),
+                    meta: {
+                        title: "项目准备"
+                    }
+                },
+                {
+                    path: 'training',
+                    name: 'ProjectTraining',
+                    component: () => import("@/views/project/training/index.vue"),
+                    meta: {
+                        title: "训练"
+                    }
+                },
+                {
+                    path: 'models',
+                    name: 'ProjectModels',
+                    component: () => import("@/views/project/models/index.vue"),
+                    meta: {
+                        title: "模型"
+                    }
+                },
+                {
+                    path: 'makes',
+                    name: 'ProjectMakes',
+                    component: () => import("@/views/project/makes/index.vue"),
+                    meta: {
+                        title: "制作"
+                    }
+                }
+            ]
+        },
+        {
             path: '/train',
             name: 'Train',
             component: () => import("@/views/train/index.vue"),
+            meta: {
+                title: "训练"
+            },
             children: [
                 {
                     path: '',
                     name: 'TrainIndex',
                     component: () => import("@/views/train/index/index.vue"),
-                    meta: {
-                        title: "项目列表"
-                    }
                 },
                 {
                     path: 'image',
