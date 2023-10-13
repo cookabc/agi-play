ffa67f6a2341cab0375ec0ce12e79c7e78ac60e3
cookabc <chuangcius@gmail.com>
2023-10-13 15:02:25 +0800

    update API: /session/name

--- server/api/session/name.ts
+++ server/api/session/name.ts
@@ -1,6 +1,13 @@
+import {updateRow} from "~/database";
+import {Response} from "~/types/response";
+
 export default defineEventHandler(async (event) => {
-    return await $fetch(useRuntimeConfig().baseURL + '/api/v2/session', {
-        method: 'PUT',
-        body: await readBody(event)
-    })
+    const body = await readBody(event) as { id: string; name: string };
+    let sql = `
+        UPDATE session
+        SET name = '${body.name}'
+        WHERE id = ${body.id} AND deleted_at IS NULL
+    `
+    await updateRow(sql);
+    return {code: 0, data: {}, message: 'OK'} as Response;
 });
