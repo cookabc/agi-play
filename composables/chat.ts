554bd578492405b17a262137b9e99726a4dfd1ce
cookabc <chuangcius@gmail.com>
2023-10-13 14:53:22 +0800

    add: baseURL

--- composables/chat.ts
+++ composables/chat.ts
@@ -3,6 +3,7 @@ import {Response} from "~/types/response";
 
 export async function getChatList(sessionId: string) {
     return await $fetch('/chat/list', {
+        baseURL: useRuntimeConfig().public.apiBase,
         method: 'POST',
         query: {sessionId},
     }) as Response
@@ -11,6 +12,7 @@ export async function getChatList(sessionId: string) {
 // 对话
 export async function makeChat(body: any) {
     return await $fetch('/chat/do', {
+        baseURL: useRuntimeConfig().public.apiBase,
         method: 'POST',
         body: body,
     }) as Response
--- composables/prompt.ts
+++ composables/prompt.ts
@@ -2,10 +2,13 @@ import {Response} from "~/types/response";
 
 export async function getPromptList(name: string) {
     return await $fetch('/prompt/list', {
+        baseURL: useRuntimeConfig().public.apiBase,
         query: {name: name}
     }) as Response
 }
 
 export async function getDefaultPrompts() {
-    return await $fetch('/prompt/default') as Response
+    return await $fetch('/prompt/default', {
+        baseURL: useRuntimeConfig().public.apiBase,
+    }) as Response
 }
