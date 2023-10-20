2a6ca60ba45fc7b904b1ee885c81d9984fff2493
cookabc <chuangcius@gmail.com>
2023-10-20 17:30:16 +0800

    fix API: /prompt/default

--- server/api/prompt/default.ts
+++ server/api/prompt/default.ts
@@ -2,20 +2,6 @@ import {getRows} from "~/database";
 import {Response} from "~/types/response";
 
 export default defineEventHandler(async () => {
-    let sql = `
-        SELECT p.id,
-            pc.name    as category,
-            pc."order" as category_order,
-            p.title,
-            p.summary,
-            p.prompt,
-            p."order"  as prompt_order,
-            p.hot,
-            FALSE
-        FROM prompt p
-                 JOIN prompt_category pc ON p.category_id = pc.id
-        WHERE p.hot = true
-          AND p.deleted_at is null;
-    `
+    let sql = 'SELECT id, prompt, assistant FROM prompt_default WHERE deleted_at is null;'
     return {code: 0, data: await getRows(sql), message: 'OK'} as Response;
 });
