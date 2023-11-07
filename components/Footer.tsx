45f5c6dbf147e070c720bd79768b3b59d0f0e68d
cookabc <chuangcius@gmail.com>
2023-11-07 11:11:29 +0800

    clean code

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,7 @@
-import React from "react";
-
 export default function FooterLayout() {
     const currentYear = new Date().getFullYear();
     return (
-        <footer
-            className="flex items-center justify-around flex-wrap text-[14px] py-5">
+        <footer className="flex items-center justify-center text-[14px] py-5">
             <p className="my-0">
                 Copyright © <span>{currentYear}</span> AGI School
             </p>
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function HeaderLayout() {
     return (
         <header className="bg-slate-100 px-5 flex items-center">
-            <a href="/" className="mr-5 font-900 text-2xl f-c-c">
+            <a href="/" className="mr-5 f-c-c">
                 <Image src="logo.svg" width={250} height={60} alt="Logo"/>
             </a>
         </header>
