7674eab7e4a8f72e5b12478537d688102ad59073
cookabc <chuangcius@gmail.com>
2023-11-07 13:07:50 +0800

    clear content

--- app/page.tsx
+++ app/page.tsx
@@ -14,35 +14,12 @@ interface Section {
 
 const sections: Section[] = [
     {
-        name: '机器学习训练平台',
-        children: [
-            {
-                title: '图片项目',
-                url: '/train/',
-                imgUrl: '/assets/images/image_classification.png',
-                description: '从问题定义、收集数据、训练模型，解决问题的实现一个完整图像分类模型及其应用，让你可以不用写一行代码，体验AI是如何分辨不同的图像的。在实验过程中，通过不断地收集数据、训练模型的迭代过程，去理解数据在AI模型中所扮演的作用。'
-            },
-            {
-                title: '音频项目',
-                url: '/train/',
-                imgUrl: '/assets/images/audio_classification.png',
-                description: '从计算机如何将人的声音转化成自己可以理解的数字信号，并且通过对不同数字信号的分类模型的训练，体验AI是如何识别人类的语音的。在实验过程中，通过不同的语音指令发布给AI，以及针对噪声的处理，看看AI在识别语音的准确上会出现哪些问题。'
-            },
-            {
-                title: '姿势项目',
-                url: '/train/',
-                imgUrl: '/assets/images/pose.jpg',
-                description: 'lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ips'
-            }
-        ]
-    },
-    {
         name: '视觉工具/实验',
         children: [
             {
-                title: '人脸检测工具',
-                url: '/face/',
-                imgUrl: '/assets/images/face.png',
+                title: '图像分类游戏-Pacman',
+                url: '/pacman/',
+                imgUrl: '/assets/images/pacman.jpg',
                 description: 'lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ips'
             },
             {
@@ -52,68 +29,26 @@ const sections: Section[] = [
                 description: 'lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ips'
             },
             {
-                title: '图像分类模型应用-生活中的图像实验',
-                url: '/train/',
-                imgUrl: '/assets/images/image_classification.png',
+                title: '人脸检测工具',
+                url: '/face/',
+                imgUrl: '/assets/images/face.png',
                 description: 'lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ips'
             },
-
-            {
-                title: '图像分类模型小游戏-Pacman',
-                url: '/pacman/',
-                imgUrl: '/assets/images/pacman.jpg',
-                description: 'lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ips'
-            }
         ]
     },
     {
         name: '语音工具/实验',
         children: [
             {
-                title: '看见声波工具',
-                url: '/see-music/',
-                imgUrl: '/assets/images/audio_frequency.jpg',
-                description: 'lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ips'
-            },
-            {
-                title: '波形采样工具',
-                url: '/wave/',
-                imgUrl: '/assets/images/swid.jpg',
-                description: 'lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ips'
-            },
-            {
-                title: '声音分类模型游戏1-寻找布谷鸟',
-                url: '/cuckoo/',
-                imgUrl: '/assets/images/cuckoo.jpg',
-                description: 'lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ips'
-            },
-            {
-                title: '声音分类模型游戏2-贪食蛇',
+                title: '声音分类游戏-贪食蛇',
                 url: '/snake/',
                 imgUrl: '/assets/images/snake.jpg',
                 description: 'lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ips'
             },
             {
-                title: '语音识别教学工具',
-                url: '#',
-                imgUrl: '/assets/images/speech.png',
-                description: 'lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ips'
-            },
-            {
-                title: '个性化声音合成工具',
-                url: '#',
-                imgUrl: '/assets/images/sound_synthesis.jpg',
-                description: 'lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ips'
-            }
-        ]
-    },
-    {
-        name: 'NLP工具/实验',
-        children: [
-            {
-                title: '自然语言处理教学工具',
-                url: '/nlp/',
-                imgUrl: '/assets/images/nlp.jpeg',
+                title: '声音分类游戏-寻找布谷鸟',
+                url: '/cuckoo/',
+                imgUrl: '/assets/images/cuckoo.jpg',
                 description: 'lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ips'
             }
         ]
@@ -122,19 +57,13 @@ const sections: Section[] = [
         name: '大模型专题',
         children: [
             {
-                title: 'GPT大语言模型工具',
+                title: '语言模型工具',
                 url: 'https://chat.agischool.com.cn',
                 imgUrl: '/assets/images/chat.jpg',
                 description: 'lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ips'
             },
             {
-                title: '绘画大模型工具',
-                url: '#',
-                imgUrl: '/assets/images/chat.jpg',
-                description: 'lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ips'
-            },
-            {
-                title: '孩子涂鸦AI工具',
+                title: '绘画模型工具',
                 url: '#',
                 imgUrl: '/assets/images/chat.jpg',
                 description: 'lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ips'
