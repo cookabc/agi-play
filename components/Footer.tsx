bdf661f8129872def81d1e29cba429679ffec328
cookabc <chuangcius@gmail.com>
2023-11-07 11:05:14 +0800

    add footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 export default function FooterLayout() {
+    const currentYear = new Date().getFullYear();
     return (
-        <>FooterLayout</>
-    )
+        <footer
+            className="flex items-center justify-around flex-wrap text-[14px] py-5">
+            <p className="my-0">
+                Copyright © <span>{currentYear}</span> AGI School
+            </p>
+        </footer>
+    );
 }
