7fe2484f21138a8aeb257fd88004bb3fbfcf097e
cookabc <chuangcius@gmail.com>
2023-11-07 10:49:57 +0800

    add Logo in Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Image from 'next/image'
 
 export default function HeaderLayout() {
     return (
-        <>HeaderLayout</>
+        <header className="bg-slate-100 px-5 flex items-center">
+            <a href="/" className="mr-5 font-900 text-2xl f-c-c">
+                <Image src="logo.svg" width={250} height={60} alt="Logo"/>
+            </a>
+        </header>
     )
 }
