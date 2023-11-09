a9439c55555238bb7765350fefff5baa9b0f5e49
cookabc <chuangcius@gmail.com>
2023-11-09 11:47:52 +0800

    add router: /cuckoo

--- next.config.js
+++ next.config.js
@@ -3,9 +3,13 @@ const nextConfig = {
     rewrites: async () => {
         return [
             {
+                source: '/cuckoo',
+                destination: '/cuckoo/index.html',
+            },
+            {
                 source: '/pose',
                 destination: '/pose/index.html',
-            }
+            },
         ];
     },
 };
