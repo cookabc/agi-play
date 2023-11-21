2c428df51334d82d662dce86337f815158fed310
cookabc <chuangcius@gmail.com>
2023-11-21 11:57:08 +0800

    add priority for Image

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ export default function HeaderLayout() {
         <header className="header fixed top-0 px-5 z-20 bg-white w-full h-[64px] flex"
                 style={{borderBottom: '1px solid var(--ant-primary-color)'}}>
             <a href="/" className="mr-5 font-900 text-2xl flex items-center justify-center">
-                <Image src="logo.svg" height={60} width={250} alt="Logo"/>
+                <Image src="logo.svg" height={60} width={250} alt="Logo" priority/>
             </a>
         </header>
     )
