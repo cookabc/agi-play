4480410eb48653a62c4257638d45785f69a386eb
cookabc <chuangcius@gmail.com>
2023-11-21 16:08:31 +0800

    use next/Link to replace tag a

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
+import Link from "next/link";
 
 export default function HeaderLayout() {
     return (
         <header className="header fixed top-0 px-5 z-20 bg-white w-full h-[64px] flex"
                 style={{borderBottom: '1px solid var(--ant-primary-color)'}}>
-            <a href="/" className="mr-5 font-900 text-2xl flex items-center justify-center">
+            <Link href="/" className="mr-5 font-900 text-2xl flex items-center justify-center">
                 <Image src="logo.svg" height={60} width={250} alt="Logo" priority/>
-            </a>
+            </Link>
         </header>
     )
 }
