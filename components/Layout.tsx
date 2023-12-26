7c9783d517eaf17f3e630ca0636703db55bfa55d
cookabc <chuangcius@gmail.com>
2023-12-26 18:44:40 +0800

    clean code

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,9 +2,9 @@
 
 import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 const inter = Inter({
     subsets: ["latin"],
@@ -18,14 +18,23 @@ export const metadata: Metadata = {
 
 const cx = (...classNames: string[]) => classNames.filter(Boolean).join(" ");
 
-
 export default function RootLayout({children}: { children: React.ReactNode }) {
+    const currentYear = new Date().getFullYear();
     return (
         <div
             className={cx(inter.variable, "font-mr bg-light dark:bg-dark") + ' flex flex-col h-screen justify-between'}>
-            <Header/>
+            <header className="header fixed top-0 px-5 z-20 bg-white w-full h-[64px] flex"
+                    style={{borderBottom: '1px solid var(--ant-primary-color)'}}>
+                <Link href="/" className="mr-5 font-900 text-2xl flex items-center justify-center">
+                    <Image src="logo.svg" height={60} width={250} alt="Logo" priority/>
+                </Link>
+            </header>
             {children}
-            <Footer/>
+            <footer className="flex items-center justify-center text-[14px] py-5">
+                <p className="my-0">
+                    Copyright © <span>{currentYear}</span> AGI School
+                </p>
+            </footer>
         </div>
     )
 }
