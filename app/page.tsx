b272b0341a016db4ca114f0fe3b19b0dccc23e58
cookabc <chuangcius@gmail.com>
2023-12-27 16:33:58 +0800

    change path for images

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface Item {
     url: string;
@@ -19,25 +20,25 @@ const sections: Section[] = [
             {
                 title: '图像分类模型训练',
                 url: '/train/image',
-                imgUrl: '/static/images/train_image_fv.png',
+                imgUrl: '/images/train_image_fv.png',
                 description: '上传图片文件或摄像头拍摄照片，进行图像分类模型训练。'
             },
             {
                 title: '声音分类模型训练',
                 url: '/train/image',
-                imgUrl: '/static/images/train_audio_fv.png',
+                imgUrl: '/images/train_audio_fv.png',
                 description: '上传音频文件或麦克风录制音频，进行音频分类模型训练。'
             },
             {
                 title: '姿势分类模型训练',
                 url: '/train/pose',
-                imgUrl: '/static/images/train_pose_fv.png',
+                imgUrl: '/images/train_pose_fv.png',
                 description: '上传图片文件或摄像头拍摄照片，进行姿势模型训练。'
             },
             {
                 title: 'AI训练场',
                 url: 'https://train-agischool.vercel.app',
-                imgUrl: '/static/images/train_audio.png',
+                imgUrl: '/images/train_audio.png',
                 description: '上传图片文件或摄像头拍摄照片，进行图像分类模型训练。'
             }
         ]
@@ -48,25 +49,25 @@ const sections: Section[] = [
             {
                 title: '人体检测',
                 url: '/pose',
-                imgUrl: '/static/images/tool_pose.png',
+                imgUrl: '/images/tool_pose.png',
                 description: 'AI是如何识别出我的动作的？看一下就明白了。'
             },
             {
                 title: '人脸检测',
                 url: '/face',
-                imgUrl: '/static/images/tool_face.png',
+                imgUrl: '/images/tool_face.png',
                 description: '一起来了解下人脸打卡机是如何实现的吧！'
             },
             {
                 title: '聊天助手',
                 url: 'https://chat-agischool.vercel.app',
-                imgUrl: '/static/images/tool_chat.png',
+                imgUrl: '/images/tool_chat.png',
                 description: 'GPT模型是如何帮助大家提高效率的？这里有好多场景等你来探索，快来试试吧！'
             },
             {
                 title: "OpenAI工具",
                 url: "/openai",
-                imgUrl: "/static/images/tool_openai.png",
+                imgUrl: "/images/tool_openai.png",
                 description: "AI是如何理解意图、识别情绪、回答问题的？这里可以找到答案。"
             }
         ]
@@ -77,25 +78,25 @@ const sections: Section[] = [
             {
                 title: '图像分类-吃豆人',
                 url: '/pacman',
-                imgUrl: '/static/images/game_pacman.png',
+                imgUrl: '/images/game_pacman.png',
                 description: '爸爸小时候用键盘和手柄玩吃豆人（Pacman）游戏，我现在用手势指挥我的吃豆人。'
             },
             {
                 title: '声音分类-贪食蛇',
                 url: '/snake',
-                imgUrl: '/static/images/game_snake.png',
+                imgUrl: '/images/game_snake.png',
                 description: '用手势指挥已经不酷了，让我用声音来控制贪食蛇吧，好Cool!'
             },
             {
                 title: '声音分类-寻找布谷鸟',
                 url: '/cuckoo',
-                imgUrl: '/static/images/game_cuckoo.jpg',
+                imgUrl: '/images/game_cuckoo.jpg',
                 description: '亚马逊森林中，一种布谷鸟就要灭绝了，我们来帮助科学家找到布谷鸟，保护好它们吧。'
             },
             {
                 title: '强化学习-井字棋',
                 url: '/tic-tac-toe',
-                imgUrl: '/static/images/game_tic-tac-toe.png',
+                imgUrl: '/images/game_tic-tac-toe.png',
                 description: '让我们一同来解密AI是如何学会下棋的吧！'
             },
         ]
@@ -117,32 +118,30 @@ export default function Home() {
                             {section.children.map((item, itemIdx) => (
                                 <div key={itemIdx}
                                      className="2xl:w-1/4 xl:w-1/4 lg:w-1/3 md:w-1/2 sm:w-full w-full px-4">
-                                    <a href={item.url}
-                                       className="block h-full border-[1px] rounded-xl hover:shadow-xl"
-                                       target="_blank">
+                                    <Link href={item.url} target="_blank"
+                                          className="block h-full border-[1px] rounded-xl hover:shadow-xl">
                                         <div className="rounded-xl">
                                             <div className="w-full h-[200px] relative">
                                                 <Image
+                                                    fill
                                                     src={item.imgUrl}
                                                     alt={item.title}
                                                     className="object-cover rounded-ss-xl rounded-se-xl"
-                                                    fill
                                                     sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                                                 />
                                             </div>
                                             <div className="p-[24px] rounded-xl">
-                                                <div
-                                                    className="mb-2 text-ellipsis text-[var(--ant-primary-color)] font-bold"
-                                                    title={item.title}>
+                                                <div title={item.title}
+                                                     className="mb-2 text-ellipsis text-[var(--ant-primary-color)] font-bold">
                                                     {item.title}
                                                 </div>
-                                                <div className="line-clamp-4 text-sm text-slate-500"
-                                                     title={item.description}>
+                                                <div title={item.description}
+                                                     className="line-clamp-4 text-sm text-slate-500">
                                                     {item.description}
                                                 </div>
                                             </div>
                                         </div>
-                                    </a>
+                                    </Link>
                                 </div>
                             ))}
                         </div>
