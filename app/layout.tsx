a08ec6c01db8765e4a559d74d6f7cec9cf32319a
cookabc <chuangcius@gmail.com>
2023-12-27 16:14:33 +0800

    use app router to take place pages router

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
-'use client'
-
-import type {Metadata} from 'next'
-import {Inter} from 'next/font/google'
+import React from "react";
+import {Metadata} from "next";
+import {Inter} from 'next/font/google';
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import "@/app/style/globals.css";
+
+export const metadata: Metadata = {
+    title: 'AGISchool - AGI学园 - AI教学平台',
+    metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 const inter = Inter({
     subsets: ["latin"],
@@ -12,15 +16,12 @@ const inter = Inter({
     variable: "--font-in",
 });
 
-export const metadata: Metadata = {
-    title: 'AGISchool - AGI学园 - AI教学平台',
-}
-
 const cx = (...classNames: string[]) => classNames.filter(Boolean).join(" ");
-
 export default function RootLayout({children}: { children: React.ReactNode }) {
     const currentYear = new Date().getFullYear();
     return (
+        <html lang="en">
+        <body className={inter.className}>
         <div
             className={cx(inter.variable, "font-mr bg-light dark:bg-dark") + ' flex flex-col h-screen justify-between'}>
             <header className="header fixed top-0 px-5 z-20 bg-white w-full h-[64px] flex"
@@ -36,5 +37,7 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
                 </p>
             </footer>
         </div>
-    )
+        </body>
+        </html>
+    );
 }
