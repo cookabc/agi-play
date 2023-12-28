87d4d63e8000d059a190e9b0878b82a5d4f3e736
cookabc <chuangcius@gmail.com>
2023-12-28 11:05:13 +0800

    change tailwind.config.js -> tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type {Config} from 'tailwindcss';
+
+const config: Config = {
     content: [
         './app/**/*.{js,ts,jsx,tsx,mdx}',
         './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -14,5 +15,10 @@ module.exports = {
             },
         },
     },
-    plugins: [],
-}
+    plugins: [
+        require('@tailwindcss/forms'),
+        require("@headlessui/tailwindcss")
+    ],
+};
+
+export default config;
