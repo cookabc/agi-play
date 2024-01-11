9a231b15bc3f8abf53a9d28c5feb3fdaef4cc592
cookabc <chuangcius@gmail.com>
2024-01-11 16:59:40 +0800

    add response for page: /chat

--- app/(routes)/(tools)/chat/page.tsx
+++ app/(routes)/(tools)/chat/page.tsx
@@ -24,21 +24,46 @@ export default function Page() {
                     </div>
                 )}
                 {!loading && messages.length > 0 && messages.map((message: any) => {
-                    return (<div className="group message-item w-full" key={message.id}>
-                        <div
-                            className="flex p-4 gap-2 text-base md:gap-6 md:max-w-2xl lg:max-w-[38rem] xl:max-w-5xl md:py-4 lg:px-0 m-auto">
-                            <div className="flex-shrink-0 flex flex-col relative items-end">
-                                <div className="w-10">
-                                    <Image src="/images/chat/people.jpg" className="w-full h-full object-cover" alt=""/>
+                    return (<div key={message.id}>
+                        <div className="group message-item w-full">
+                            <div
+                                className="flex p-4 gap-2 text-base md:gap-6 md:max-w-2xl lg:max-w-[38rem] xl:max-w-5xl md:py-4 lg:px-0 m-auto">
+                                <div className="flex-shrink-0 flex flex-col relative items-end">
+                                    <div className="w-10">
+                                        <Image src="/images/chat/people.jpg" className="w-full h-full object-cover"
+                                               alt="" width={40} height={40}/>
+                                    </div>
+                                </div>
+                                <div
+                                    className="message-content relative bg-[var(--ant-primary-color-3)] flex flex-col gap-1 md:gap-3 p-4 lg:w-[calc(100%-115px)] rounded-xl">
+                                    <TextComponent text={message.prompt}/>
+                                    <div className="flex">
+                                        <button type="button" className="ml-auto"
+                                                onClick={() => onCopy(message.prompt)}>
+                                            <CopyOutlined/>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
+                        </div>
+                        <div className="group message-item w-full">
                             <div
-                                className="message-content relative bg-[var(--ant-primary-color-3)] flex flex-col gap-1 md:gap-3 p-4 lg:w-[calc(100%-115px)] rounded-xl">
-                                <TextComponent text={message.prompt}/>
-                                <div className="flex">
-                                    <button type="button" className="ml-auto" onClick={() => onCopy(message.prompt)}>
-                                        <CopyOutlined/>
-                                    </button>
+                                className="flex p-4 gap-2 text-base md:gap-6 md:max-w-2xl lg:max-w-[38rem] xl:max-w-5xl md:py-4 lg:px-0 m-auto">
+                                <div className="flex-shrink-0 flex flex-col relative items-end">
+                                    <div className="w-10">
+                                        <Image src="/images/chat/ai_robot.svg" className="w-full h-full object-cover"
+                                               alt="" width={40} height={40}/>
+                                    </div>
+                                </div>
+                                <div
+                                    className="message-content relative flex w-[calc(100%-50px)] flex-col lg:w-[calc(100%-115px)] bg-white p-4 rounded-xl">
+                                    <TextComponent text={message.response}/>
+                                    <div className="flex">
+                                        <button type="button" className="ml-auto" title="复制"
+                                                onClick={() => onCopy(message.response)}>
+                                            <CopyOutlined/>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
