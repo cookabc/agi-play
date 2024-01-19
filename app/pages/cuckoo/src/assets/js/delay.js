30ab26ffe101ae1824f436f2950263cfcc5b06df
cookabc <chuangcius@gmail.com>
2024-01-19 17:55:54 +0800

    remove source code pages: cuckoo & pacman & snake

--- app/pages/cuckoo/src/assets/js/delay.js
+++
@@ -1,7 +0,0 @@
-export default function delay(ms) {
-    return new Promise((resolve, reject) => {
-        const timer = setTimeout(() => {
-            resolve(timer)
-        }, ms)
-    })
-}
--- app/pages/snake/src/assets/js/index.js
+++
@@ -1,4 +0,0 @@
-export * from './ui'
-export * from './model'
-export * from './upload'
-export * from './snake'
