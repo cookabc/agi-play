f4025d1bf9b0068b1b1d0cee391a08e34944b475
cookabc <chuangcius@gmail.com>
2024-02-07 10:42:43 +0800

    add UI: clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import {Metadata} from "next";
+import {ClerkProvider, UserButton} from "@clerk/nextjs";
 import {Inter} from 'next/font/google';
+import {Metadata} from "next";
+import {shadesOfPurple} from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
 import "@/app/styles/globals.css";
@@ -18,17 +20,17 @@ const inter = Inter({
 
 const cx = (...classNames: string[]) => classNames.filter(Boolean).join(" ");
 export default function RootLayout({children}: { children: React.ReactNode }) {
-    const currentYear = new Date().getFullYear();
-    return (
+    return (<ClerkProvider appearance={{baseTheme: shadesOfPurple}}>
         <html lang="en">
         <body className={inter.className}>
         <div
             className={cx(inter.variable, "font-mr bg-light dark:bg-dark") + ' flex flex-col h-screen justify-between'}>
-            <header className="header fixed top-0 px-5 z-20 bg-white w-full h-[64px] flex"
+            <header className="header fixed top-0 px-5 z-20 bg-white w-full h-[64px] flex justify-between items-center"
                     style={{borderBottom: '1px solid var(--ant-primary-color)'}}>
                 <Link href="/" className="mr-5 font-900 text-2xl flex items-center justify-center">
                     <Image src="logo.svg" height={60} width={250} alt="Logo" priority/>
                 </Link>
+                <UserButton afterSignOutUrl="/"/>
             </header>
             <div className="relative h-full w-full">
                 {children}
@@ -36,5 +38,5 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
         </div>
         </body>
         </html>
-    );
+    </ClerkProvider>);
 }
